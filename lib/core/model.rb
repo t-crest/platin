dac9fb6c746d92280e579664464d906f5bba3018
Simon Schuster <git@rationality.eu>
2017-06-27 10:01:04 +0200

    Model: Bugfix: Ensure correct undo in case of faulty modelfacts
    The presence of faulty modelfacts resulted in incomplete undo information and
    therefore an exception. This is fixed in this commit by emitting and
    continuously updating undo informations on an per-modelfact basis.

--- lib/core/model.rb
+++ lib/core/model.rb
@@ -61,23 +61,29 @@ class Model
   end
 
   def evaluate(pml, modelfacts)
+    # Undo information
+    done = []
+    @stack << done
+
     facts = modelfacts.map {|fact| fact.to_fact(pml, self)}.compact
+
     facts.select{|f| f.kind_of?(FlowFact)}.each do |ff|
       pml.flowfacts.add(ff)
+      done << ff
     end
     facts.select{|f| f.kind_of?(ValueFact)}.each do |vf|
       pml.flowfacts.add(ff)
+      done << vf
     end
     facts.select{|f| f.kind_of?(PMLMutation)}.each do |mt|
       mt.mutate(pml)
+      done << mt
     end
 
-    # Undo information
-    @stack << facts
     self
   end
 
-  # This is actually a whole lot more fragile than ideal PMLMutations do mutate
+  # This is actually a whole lot more fragile than ideal as PMLMutations do mutate
   # the machine_functions, and those mutations actually persist across
   # pml.with_temporary_sections([:machine_functions]). Therefore we have to
   # explicitly undo them. Fragile as hell.
