7d50a9e6b12cafbd9bcfcffc5edf49512b24f2cf
Simon Schuster <git@rationality.eu>
2017-09-16 22:02:26 +0200

    ILP-Visualisation: Bugfix: Correct underflow handling with linehighlight

--- lib/tools/visualisationserver.rb
+++ lib/tools/visualisationserver.rb
@@ -242,7 +242,7 @@ class Server
         out = code[from, to - from].join("\n")
       end
       resp.content_type = 'application/json'
-      resp.body = JSON.generate({:from => from, :to => to, :code => out})
+      resp.body = JSON.generate({:from => from + 1, :to => to + 1, :code => out})
       raise WEBrick::HTTPStatus::OK
     end
   end
