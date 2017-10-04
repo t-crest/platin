8bf87f80e5d5f2c195de3fdbe25733650cb40980
Peter Waegemann <waegemann@cs.fau.de>
2017-10-04 19:17:12 +0200

    focus on proving optimality, set bounds to avoid numeric instability

--- lib/ext/gurobi.rb
+++ lib/ext/gurobi.rb
@@ -11,6 +11,15 @@ class GurobiILP < ILP
   # Tolarable floating point error in objective
   def initialize(options = nil)
     super(options)
+
+    @lines_thread0 = []
+    @backlock_idx0 = 0
+
+    @lines_thread1 = []
+    @backlock_idx1 = 0
+
+    @lines_thread2 = []
+    @backlock_idx2 = 0
   end
   # run solver to find maximum cost
   def solve_max
@@ -97,6 +106,12 @@ class GurobiILP < ILP
     end
     # We could put the bounds in as constraints, but bounds should be faster
     lp.puts("Bounds")
+
+    # we are in big numeric trouble if we do not put any bounds at all
+    @variables.each do |v|
+      lp.puts(" #{varname(index(v))} <= 100000")
+    end
+
     @constraints.each do |constr|
       # Bounds must have the form '[-1,1] x <= rhs'
       next unless constr.bound?
@@ -143,13 +158,59 @@ class GurobiILP < ILP
     end
   end
 
+  def thread0(lp, sol)
+    out = IO.popen("gurobi_cl MIPFocus=1 ResultFile=#{sol} #{lp}")
+    @lines_thread0 = []
+    backlock_idx0 = 0
+    while line = out.gets do
+      @lines_thread0.push(line)
+      if @lines_thread0.length > 10 or @options.verbose
+        while backlock_idx < @lines_thread0.length do
+          puts(@lines_thread0[backlock_idx0])
+          backlock_idx0 += 1
+        end
+      end
+    end
+  end
+
+  def thread1(lp, sol)
+    out = IO.popen("gurobi_cl MIPFocus=2 ResultFile=#{sol} #{lp}")
+    @lines_thread1 = []
+    backlock_idx1 = 0
+    while line = out.gets do
+      @lines_thread1.push(line)
+      if @lines_thread1.length > 40 or @options.verbose
+        while backlock_idx < @lines_thread1.length do
+          puts(@lines_thread1[backlock_idx1])
+          backlock_idx1 += 1
+        end
+      end
+    end
+  end
+
+  def thread2(lp, sol)
+    out = IO.popen("gurobi_cl MIPFocus=3 ResultFile=#{sol} #{lp}")
+    @lines_thread2 = []
+    backlock_idx2 = 0
+    while line = out.gets do
+      @lines_thread2.push(line)
+      if @lines_thread2.length > 40 or @options.verbose
+        while backlock_idx < @lines_thread2.length do
+          puts(@lines_thread2[backlock_idx2])
+          backlock_idx2 += 1
+        end
+      end
+    end
+  end
+
   def solve_lp(lp, sol)
+    lines = []
     # MIPFocus=1: focus on finding feasible solutions
+    # MIPFocus=2: focus on proving optimality
     # MIPFocus=3: focus on improving the bound
     # (unused: MIPGap=0.03  => stop at 3% gap between incubent and best bound)
     # (unused: TimeLimit=600 => terminate after 600 seconds)
-    out = IO.popen("gurobi_cl MIPFocus=3 ResultFile=#{sol} #{lp}")
-    lines = []
+    out = IO.popen("gurobi_cl MIPFocus=2 ResultFile=#{sol} #{lp}")
     backlock_idx = 0
     while line = out.gets do
       lines.push(line)
@@ -160,6 +221,54 @@ class GurobiILP < ILP
         end
       end
     end
+
+#    th0 = Thread.new { thread0(lp, sol) }
+#    th1 = Thread.new { thread1(lp, sol) }
+#    th2 = Thread.new { thread2(lp, sol) }
+
+#    th0.run
+#    th1.run
+#    th2.run
+#    while true
+#      if th0.status == false
+#        # thread 0 terminated normally
+#        puts "th1 won!"
+#        Thread.kill(th1)
+#        # th1.exit
+#        Thread.kill(th2)
+##        th2.exit
+##        th1.join
+##        th2.join
+#        lines = @lines_thread0
+#        break
+#      end
+#      if th1.status == false
+#        puts "th1 won!"
+#        Thread.kill(th0)
+#        #th0.exit
+#        Thread.kill(th2)
+#        #th1.exit
+##        th0.join
+##        th2.join
+#        lines = @lines_thread1
+#        break
+#      end
+#      if th2.status == false
+#        puts "th2 won!"
+#        Thread.kill(th0)
+#        th0.exit
+#        Thread.kill(th1)
+#        th1.exit
+##        th0.join
+##        th1.join
+#        lines = @lines_thread2
+#        break
+#      end
+
+#      # TODO condition variables would be better here
+#      # sleep 1
+#    end
+
     # Detect error messages
     return "Gurobi terminated unexpectedly (#{$?.exitstatus})" if $? and $?.exitstatus > 0
     lines.each do |line|
